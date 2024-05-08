8abb9d632b2857be26c91db6f1aea82ee03c3a9a
Luis Pedro Lira <luispelirak130@gmail.com>
2024-05-07 22:30:01 -0600

    Base archivo main

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,33 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Error al procesar el archivo: " + e.getMessage());
         }
+        //------------------------------------------------------------
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false; // Bandera para salir del ciclo
 
+        while (!salir) {
+            System.out.println("------ MENU ------");
+            System.out.println("1. Comprimir archivo\n2. Descomprimir archivo\n3. Salir");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+            
+            
+            switch (opcion) {
+                case 1:
+                    comprimir(); // Llamar al método de compresión
+                    break;
+                case 2:
+                    descomprimir(); // Llamar al método de descompresión
+                    break;
+                case 3:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        }
+        scanner.close();
             // Escribir los códigos de Huffman en un archivo
             /*StringBuilder codigosHuffman = new StringBuilder();
             for (Map.Entry<Character, String> entry : codigos.entrySet()) {
@@ -23,5 +49,13 @@ public class Main {
             lector.write("codigos.huff", codigosHuffman.toString());*/
 
             System.out.println("Compresión y descompresión completadas con éxito.");
+        }
+
+        public static void comprimir(){
+
+        }
+
+        public static void descomprimir(){
+
         }
     }
